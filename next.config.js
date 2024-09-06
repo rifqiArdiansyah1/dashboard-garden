5d8f65bcea5552c62ba35b472710039b0197bb52
Moh. Rifqi Dwi Ardiansyah <150460933+rifqiArdiansyah1@users.noreply.github.com>
2024-09-07 06:52:01 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-
+  basePath: '/mantap-garden',
+  trailingSlash: true,
 }
 
-module.exports = nextConfig;
+module.exports = nextConfig
